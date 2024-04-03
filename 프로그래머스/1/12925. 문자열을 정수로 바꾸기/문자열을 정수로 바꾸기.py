9141908744cb64f0dda5465ac3960105a2ea5a4f
journeythrunrun <164328543+journeythrunrun@users.noreply.github.com>
2024-04-03 21:39:47 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -0,0 +1,8 @@
+# 문자열을 정수로 바꾸기_https://school.programmers.co.kr/learn/courses/30/lessons/12925
+def solution(s):
+    # 부호_int()가 부호도 해결 ((X_앞에 한 개만 그걸로 받고 ))
+    # s='-123'
+    
+    answer = int(s)
+
+    return answer
